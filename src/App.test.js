982b0a8de19a1cffe587f2228b10a83a2f02ab6d
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2017-12-12 16:57:43 +0100

    Revert "Test if test results are accidentally cached"
    This reverts commit 67aee5b49a918af6dca3e7bab321a337f2152f1e.

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-});
-
-it('works', () => {
-    expect(2).toBe(2);
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
