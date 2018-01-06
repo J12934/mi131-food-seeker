064a7a15e83a1f0ed07ce3e1d2514e4b709fccba
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-01-06 18:54:11 +0100

    Added tests for restaurant resolvers

---
+++ src/server/resolvers/restaurant.test.js
@@ -0,0 +1,42 @@
+const { Query: { search, typeahead, restaurant } } = require('./restaurant');
+const SearchEventEmitter = require('../events/SearchEventEmitter');
+
+jest.mock('../services/RestaurantAPI');
+
+describe('restaurant resolvers', () => {
+    describe('search()', () => {
+        it('retuns expected result', async () => {
+            const result = await search(null, {
+                term: 'burger',
+                coordinates: {
+                    latitude: 54,
+                    longitude: 11,
+                },
+            });
+
+            expect(result).toMatchSnapshot();
+        });
+    });
+
+    describe('typeahead()', () => {
+        it('retuns expected result', async () => {
+            const result = await typeahead(null, {
+                term: 'bur',
+                coordinates: {
+                    latitude: 54,
+                    longitude: 11,
+                },
+            });
+
+            expect(result).toMatchSnapshot();
+        });
+    });
+
+    describe('restaurant()', () => {
+        it('retuns expected result', async () => {
+            const result = await restaurant(null, { id: 'johns-burgers-kiel' });
+
+            expect(result).toMatchSnapshot();
+        });
+    });
+});
--- src/server/services/__mocks__/RestaurantAPI.js
+++ src/server/services/__mocks__/RestaurantAPI.js
@@ -1,7 +1,37 @@
 module.exports = class YelpAPIMock {
-    search() {
-        return Promise.resolve({
-            search: { business: [{ name: 'resturant 1' }] },
-        });
+    async search() {
+        return [
+            {
+                id: 'johns-burgers-kiel',
+                name: "John's Burgers",
+                rating: 4.5,
+                coordinates: {
+                    latitude: 54.33204,
+                    longitude: 10.12449,
+                },
+            },
+            {
+                id: 'kitty-rock-belly-full-kiel',
+                name: 'Kitty Rock Belly Full',
+                rating: 4.5,
+                coordinates: {
+                    latitude: 54.33254,
+                    longitude: 10.11822,
+                },
+            },
+        ];
+    }
+
+    async typeahead() {
+        return ['Burgers', 'Burmese', 'Fast Food'];
+    }
+
+    async restaurant() {
+        return {
+            id: 'johns-burgers-kiel',
+            name: "John's Burgers",
+            rating: 4.5,
+            coordinates: { latitude: 54.33204, longitude: 10.12449 },
+        };
     }
 };
