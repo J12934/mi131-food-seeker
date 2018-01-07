add486b78f45d30bbc3dacae66ace416cc6534ac
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-01-07 16:05:33 +0100

    Removed old & unused server directory

--- server/models/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/food_seeker', { useMongoClient: true });
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
