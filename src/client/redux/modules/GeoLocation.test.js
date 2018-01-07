acc4d5b7cf9e5df826de6bd469da528c4f01869c
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-01-07 19:22:53 +0100

    Removed unnecessary prefix from redux state

--- src/client/redux/modules/GeoLocation.test.js
+++ src/client/redux/modules/GeoLocation.test.js
@@ -3,14 +3,14 @@ import reducer, { initialState, updateLocation } from './GeoLocation';
 describe('GeoLocation', () => {
     describe('UPDATE_LOCATION', () => {
         it('should set tracking to true', () => {
-            expect(initialState.geoLocation.tracking).toBe(false);
+            expect(initialState.tracking).toBe(false);
             const state = reducer(initialState, updateLocation(10, 10));
-            expect(state.geoLocation.tracking).toBe(true);
+            expect(state.tracking).toBe(true);
         });
 
         it('should set coordinates', () => {
             const state = reducer(initialState, updateLocation(42, 3));
-            expect(state.geoLocation.coordinates).toEqual({
+            expect(state.coordinates).toEqual({
                 latitude: 42,
                 longitude: 3,
             });
