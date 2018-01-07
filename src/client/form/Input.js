cf29e185bfe612ad60d2d58ef28ac428a4a4d420
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-01-07 14:19:24 +0100

    Added styled-components as styling solution

---
+++ src/client/form/Input.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export default styled.input`
+    background-color: #fafafa;
+    border: none;
+    border-radus: 2px;
+    padding: 12px;
+
+    font-size: 24px;
+    font-weight: bold;
+`;
