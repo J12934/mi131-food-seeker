7eb3ef0556db97b70a0f5312c8faf205f120be02
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-01-07 19:43:02 +0100

    Added enzyme for component tests

---
+++ src/setupTests.js
@@ -0,0 +1,5 @@
+// See https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#testing-components
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
