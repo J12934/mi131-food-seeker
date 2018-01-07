d6617f84f20248802c4c6600e892af607634e7c0
Jannik Hollenbach <Jannik.Hollenbach@iteratec.de>
2018-01-07 13:49:32 +0100

    Removed socket.io from client

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import io from 'socket.io-client';
 
 import './client/index.css';
 import App from './client/App';
 import registerServiceWorker from './client/registerServiceWorker';
-
-const socket = io('/');
-
-socket.emit('query', {
-    term: 'Burgers',
-    location: 'Kiel',
-});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
